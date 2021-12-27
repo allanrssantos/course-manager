42b0b13c8719727b37fbc4a4da9f16e198d668d8
Allan <allans@brq.com>
2021-12-27 11:57:53 -0300

    fix: correção das rotas.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { FormsModule } from '@angular/forms';
+import { CourseListComponent } from './courses/course-list.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
@@ -9,6 +11,7 @@ import { AppComponent } from './app.component';
 import { CourseModule } from './courses/course.module';
 import { CoreModule } from './core/core.module';
 import { Error404Compoennt } from './core/component/error-404/error-404.component';
+import { CourseInfoComponent } from './courses/course-info.component';
 
 @NgModule({
   declarations: [
@@ -17,11 +20,19 @@ import { Error404Compoennt } from './core/component/error-404/error-404.componen
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
       {
+        path: 'courses', component: CourseListComponent
+      },
+      {
+        path: 'courses/info/:id', component: CourseInfoComponent
+      },
+      {
         path: '', redirectTo: 'courses', pathMatch: 'full'
-      }, {
+      }, 
+      {
         path: '**', component: Error404Compoennt
       }
     ]),
